d4234fb08ab3cc56cb77977acc6d4a5dd62f7f9c
Igor Ovsiannikov <garek@mail.ru>
2019-07-08 19:09:44 +0900

    feat: prettify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-const [a,p,b,c] = process.argv.slice(2);
+const [a, p, b, c] = process.argv.slice(2);
 
-if ([a,p,b].filter(Boolean).length !== 3) {
-  throw new Error("Please give me three characters!");
+if ([a, p, b].filter(Boolean).length !== 3) {
+  throw new Error('Please give me three characters!');
 }
 
 if (Number.isFinite(c) || c < 0 || c > 100) {
-  throw new Error("Please, give me a number in [0, 100] as well")
+  throw new Error('Please, give me a number in [0, 100] as well');
 }
 
 const PROGRESS_CHAR_LEN = 25;
-const progress = Math.ceil(c / 100 * 25);
+const progress = Math.ceil((c / 100) * 25);
 
 if (progress <= 0) {
   console.log('💥');
